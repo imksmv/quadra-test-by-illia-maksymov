db0e0f71e6e8364ee9febff7082e89d596c5bc30
imksmv <98290303+imksmv@users.noreply.github.com>
2024-02-01 18:05:08 +0200

    Install required dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <section className="container flex items-center justify-center h-full">
-      <div>
-        <p className="uppercase">Quadra Test</p>
-        <div className="">
+      <div className="flex flex-col items-center border p-4 rounded-md">
+        <p className="uppercase mb-4">Quadra Test</p>
+        <div className="bg-primary/40 p-4 rounded-md">
+          {/* Todo ⤵ */}
           <p>1000000 - 9999999 every 100ms</p>
         </div>
       </div>
