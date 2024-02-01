91743a143baaa6063eaa4ffe7af0d36d1463f921
imksmv <98290303+imksmv@users.noreply.github.com>
2024-02-01 12:57:09 +0200

    Add colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <section></section>
+  return (
+    <section className="container flex items-center justify-center h-full">
+      <div>
+        <p className="uppercase">Quadra Test</p>
+        <div className="">
+          <p>1000000 - 9999999 every 100ms</p>
+        </div>
+      </div>
+    </section>
+  )
 }
